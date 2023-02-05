7763d94f703d0af6027eb2d355db5aa8369a1c1a
fullmetalashley <73601224+fullmetalashley@users.noreply.github.com>
2023-02-05 07:57:09 -0800

    Adding in some letters and clean up.

--- GGJ 2023 Pt 2/Assets/Scripts/ClickDetection.cs
+++ GGJ 2023 Pt 2/Assets/Scripts/ClickDetection.cs
@@ -5,17 +5,21 @@ using UnityEngine;
 public class ClickDetection : MonoBehaviour
 {
     private DialogueManager theDialogue;
+    private UIManager theUI;
     private DialogueTrigger trigger;
     
     public void Start()
     {
         theDialogue = FindObjectOfType<DialogueManager>();
         trigger = this.gameObject.GetComponent<DialogueTrigger>();
+        theUI = FindObjectOfType<UIManager>();
     }
     
     void OnMouseDown()
     {
         if (theDialogue.dialogueActive) return;
+        if (theUI.uiOpen) return;
+        
         if (trigger != null)
         {
             trigger.TriggerDialogue();
